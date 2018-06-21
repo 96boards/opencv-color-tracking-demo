8d0c2990673075adc29daca7619bf66e40a504bc
Don Harbin <don.harbin@gmail.com>
2018-06-21 09:18:03 -0700

    Fixed many bugs and tuned counting algorithm

--- counter/MM.py
+++ counter/MM.py
@@ -28,54 +28,81 @@ import numpy as np
 from random import randint
 import time
 
-
+##########
+# Class: For each color
+##########
 class M_and_M:
-    # Keeps track of individual mm's that are in the view as MM_Objects.
-    trackPrevious = []    
-    trackNext = []   
 
     objectID = 0
+
+    # Tuning parameters:
+    # The fact that objects can move at different speeds are what will keep this from being 100% accurate.
+    # So tune as close as possible for your solution. Notes to increase precision:
+    #   1) the faster the video capture/OpenCV processing loop in track.py, the more accurate the results.
+    #   2) a faster camera.  Used 30 fps one here.  
+    #   3) the more predictable the speeds of the objects, the better. Turns out Peanut M&Ms have quite a 
+    #      variant in shapes that impact consitant velocity/acceleration.
     max_distance = 300   # Max pixels to assure it's not the same mm object since last pass.
-    min_valid_yCoordinate = 200
+    min_distance = 100   # Min
+    min_valid_yCoordinate = 40     # if below 100, assume will survive next pass
+    max_valid_yCoordinate = 300    # if abov 300, assume it's gone
 
+    ##########
+    # Class:  For each MM
+    ##########
     class MM_Object:
-        age=0
-        triggered = False      # Record if this object instance has been counted yet.
 
-        def __init__(self, xi, yi):
-            print "Debug: MM_object init x,y= ", xi,yi
+        def __init__(self, xi, yi, trigger):
+            #print "Debug: MM_object init x,y= ", xi,yi
             self.x = xi
             self.y = yi
+            self.triggered=trigger    # Record if this object instance has been counted yet.
+            self.age=0
 
         def setTrigger(self):
             self.triggered = True
-
         def getTriggerState(self):
-            print "Debug: Enter getTriggerState triggered =", self.triggered
+            #print "Debug: Enter getTriggerState triggered =", self.triggered
             return self.triggered 
+        def setTriggerState(self, trig):
+            self.triggered=trig
 
         def getX(self):
             return self.x
+        def setX(self, xi):
+            self.x=xi
+
         def getY(self):
             return self.y
+        def setY(self, yi):
+            self.y=yi
 
         def updateNewCoords(self, xn, yn):
-            print "Debug: Enter updateNewCoords xn, yn, x, y =", xn, yn, self.x, self.y
-            self.age += 1
-            print "Debug: age =", self.age
+            #print "Debug: Enter updateNewCoords xn, yn, x, y =", xn, yn, self.x, self.y
+            self.incrementAge()
             self.x = xn
             self.y = yn
 
         def getAge(self):
             return self.age
+        def setAge(self, a):
+            self.age = a
+        def incrementAge(self):
+            #print "Debug: age =", self.age
+            self.age += 1
 
-    def __init__(self, color_id, color, circle_color=[], hsv_min=[], hsv_max=[],text_location=[]):
+    def __init__(self, color_id, color, age_t, circle_color=[], hsv_min=[], hsv_max=[],text_location=[]):
+        # Keeps track of individual mm's that are in the view as MM_Objects.
+        self.trackPrevious = []    
+        self.trackNext = []   
+
         self.uid = color_id    #Unique ID
         self.color = color     #Text Color field for printing
         self.circle_color=circle_color    #The color of the circle in found Frame objects 
         self.hsv_min=hsv_min
         self.hsv_max=hsv_max
         self.text_location=text_location
+        self.age_threshold=age_t
 
         #Initializat Local Class variables
         self.count=0		#Initialize color count
@@ -119,25 +146,37 @@ class M_and_M:
         self.trackNext = []   
 
     def addTrackNext(self, xn, yn):
-        self.trackNext.append(M_and_M.MM_Object(xn,yn))
+        self.trackNext.append(M_and_M.MM_Object(xn,yn, False))
         
     def newObjectCheck(self):
-        print "Debug: Enter newObjectCheck"
+        #print "Debug: Enter newObjectCheck"
 
         # Clean up self.trackPrevious before checking for advanced existing object
         for i in self.trackPrevious:
-            print "Debug: newObjectCheck.trackPrevious"
             x=i.getX()
             y=i.getY()
-            #Assume that object has rolled out of screen if below min_valid_yCoordinate
-            if y > self.min_valid_yCoordinate or i.getTriggerState() == True:
+            print "Debug: newObjectCheck.trackPrevious y age ", y, i.getAge()
+
+            # Assume that object has rolled out of screen very quickly and never got logged 
+            if i.getAge() > self.age_threshold:
+                if i.getTriggerState() == False:
+                    print "Debug: Aged out INCREMENT COUNT"
+                    self.incrementCount()
                 self.trackPrevious.remove(i)
+            else:
+                i.incrementAge()
+
 
         if not self.trackPrevious:
-            print "Debug: newObjectCheck. No old mm's, add all as new"
+            #print "Debug: newObjectCheck. No old mm's, add all as new"
             # Add all objects in TrackNext as new mm objects
             for t in self.trackNext:
-                self.addNewObject(t.getX(),t.getY())
+                print "Debug: newObjectCheck. Process new object y = ", t.getY()
+                if t.getY() > self.min_valid_yCoordinate and t.getTriggerState() == False:
+                    print "Debug: INCREMENT COUNT"
+                    self.incrementCount()
+                    t.setTrigger()
+                self.addNewObject(t.getX(),t.getY(),t.getTriggerState())
         else:
             ##########
             # Now ready to run algorithm to check whether object is a new one or previous one that moved.
@@ -151,26 +190,55 @@ class M_and_M:
                 yp=tPrev.getY()
 
                 for tNext in self.trackNext:
+                    print "Debug: newObjectCheck tPrev y, tNext y =", yp, tNext.getY() 
                     yn = tNext.getY()
 
-                    # If not within max_distance pixels then assume new mm.
-                    if yn-yp > self.max_distance: 
+                    # If not within valid band, assume new mm.
+                    if yn > yp and abs(yn-yp) > self.max_distance and abs(yn-yp) < self.min_distance: 
                         # See if below trigger count line
+                        print "Debug: newObjectCheck max distance exceeded"
                         if tNext.getY() > self.min_valid_yCoordinate:
                             if tNext.getTriggerState() == False:
                                 self.incrementCount()
                                 tNext.setTrigger()
-                        self.addNewObject(tNext.getX(),tNext.getY())
+
+                        self.addNewObject(tNext.getX(),tNext.getY,tNext.getTriggerState())
+                        self.trackNext.remove(tNext)
                     else:  # Same mm new location
-                        tPrev = tNext
+                        print "Debug: newObjectCheck new location"
+
+                        # import pdb; pdb.set_trace()
+                        tPrev.setX(tNext.getX())
+                        tPrev.setY(tNext.getY())
+                        if tPrev.getTriggerState() == False:
+                            tPrev.setTriggerState(tNext.getTriggerState())
+                        tPrev.setAge(0)
+        
+                        self.trackNext.remove(tNext)
+
                         if tPrev.getY() > self.min_valid_yCoordinate and tPrev.getTriggerState() == False:
+                            print "Debug: newObjectCheck INCREMENT COUNT"
                             self.incrementCount()
                             tPrev.setTrigger()
-                        
-                    
-    def addNewObject(self, xn, yn):
+                        break    # Consumed the tPrev object here so go grab a new one
+
+            # Check for remaining new objects seen, process each one, and move to trackPrevious[]
+            for tNext in self.trackNext:
+                print "Debug: newObjectCheck - add remaining new objects from tNext to tPrev"
+                yp=tNext.getY()
+                xp=tNext.getX()
+                if yp > self.min_valid_yCoordinate:
+                    print "Debug: newObjectCheck cleanup INCREMENT COUNT"
+                    self.incrementCount()
+                    temp_trig=True
+                else:
+                    temp_trig=False
+
+                self.addNewObject(xp, yp, temp_trig)
+
+    def addNewObject(self, xn, yn, trigger_state):
         print "Debug: Add new MM Object"
-        self.trackPrevious.append(M_and_M.MM_Object(xn,yn))
+        self.trackPrevious.append(M_and_M.MM_Object(xn,yn,trigger_state))
 
     def appendNewCoords(self, mmObject,xn, yn):
         return
@@ -184,12 +252,12 @@ class M_and_M:
         
     # Check to see it the trigger line is being crossed
     def triggerCheck(self, x, y, y_trigger):
-        print "Debug: Enter triggerCheck x,y= ", x, y
+        #print "Debug: Enter triggerCheck x,y= ", x, y
         for i in self.track:
             if i.getTriggerState()==False: 
-                 print "Debug: triggerCheck i.getY(), y_trigger, y =", i.getY(), y_trigger, y
+                 #print "Debug: triggerCheck i.getY(), y_trigger, y =", i.getY(), y_trigger, y
                  if i.getY() < y_trigger and y_trigger < y:
-                     print "Debug: triggerCheck **** call setTrigger() ****"
+                     #print "Debug: triggerCheck **** call setTrigger() ****"
                      i.setTrigger()
                      return True
         return False
--- counter/track_mm.py
+++ counter/track_mm.py
@@ -29,20 +29,22 @@ import time
 import imutils
 import time
 
-
-kernelOp = np.ones((3,3),np.uint8)
-kernelOp1 = np.ones((7,7),np.uint8)
-kernelOp2 = np.ones((5,5),np.uint8)
-
-kernelCl = np.ones((11,11),np.uint8)
-kernelCl1 = np.ones((20,20),np.uint8)
-kernelCl2 = np.ones((25,25),np.uint8)
+# Used to tune morphologicals
+kernel3 = np.ones((3,3),np.uint8)
+kernel5 = np.ones((5,5),np.uint8)
+kernel7 = np.ones((7,7),np.uint8)
+kernel11 = np.ones((11,11),np.uint8)
+kernel15 = np.ones((15,15),np.uint8)
+kernel20 = np.ones((20,20),np.uint8)
+kernel25 = np.ones((25,25),np.uint8)
 
 #Variables
 font = cv2.FONT_HERSHEY_SIMPLEX
+
+# Tuning parameters
 mm = []
-max_p_age = 5
-min_radius = 9        # Adjust to size of m & m. Impacted by distance from camera
+max_age = 4            # a mm object only lives through max_age passes
+min_radius = 12        # Adjust to size of m & m. Impacted by distance from camera
 
 #DEBUG import pdb; pdb.set_trace() # Begin debug
 
@@ -56,102 +58,102 @@ color_id = 0          # One for each color
 redCircleColor = (0,0,255)
 redTextLocation = (10,25)
 
-red_hue_min = 158
-red_saturation_min = 107 
-red_value_min = 34 
+red_hue_min = 0
+red_saturation_min = 80 
+red_value_min = 23 
 red_hsv_min=np.array([red_hue_min,red_saturation_min,red_value_min])
 
-red_hue_max = 193
-red_saturation_max = 229 
-red_value_max = 171
+red_hue_max = 10
+red_saturation_max = 256 
+red_value_max = 110
 red_hsv_max=np.array([red_hue_max,red_saturation_max,red_value_max])
 
-mm.append(MM.M_and_M(color_id, "Red",redCircleColor, red_hsv_min,red_hsv_max,redTextLocation))  # Create Red Tracking Object
+mm.append(MM.M_and_M(color_id, "Red", max_age, redCircleColor, red_hsv_min,red_hsv_max,redTextLocation))  # Create Red Tracking Object
 
 # BLUE
 color_id += 1
 blueCircleColor = (255,0,0)
 blueTextLocation = (10,50)
 
-blue_hue_min = 84
-blue_saturation_min = 77 
-blue_value_min = 57 
+blue_hue_min = 83
+blue_saturation_min = 96 
+blue_value_min = 55 
 blue_hsv_min=np.array([blue_hue_min,blue_saturation_min,blue_value_min])
 
-blue_hue_max = 146
-blue_saturation_max = 236 
-blue_value_max = 160
+blue_hue_max = 143
+blue_saturation_max = 251 
+blue_value_max = 167
 blue_hsv_max=np.array([blue_hue_max,blue_saturation_max,blue_value_max])
 
-mm.append(MM.M_and_M(color_id, "Blue",blueCircleColor, blue_hsv_min,blue_hsv_max,blueTextLocation))  # Create Red Tracking Object
+mm.append(MM.M_and_M(color_id, "Blue", max_age, blueCircleColor, blue_hsv_min,blue_hsv_max,blueTextLocation))  # Create Red Tracking Object
 
 # ORANGE
 color_id += 1
 orangeCircleColor = (0,125,255)
 orangeTextLocation = (10,75)
 
-orange_hue_min = 4
-orange_saturation_min = 155 
-orange_value_min = 104 
+orange_hue_min = 5
+orange_saturation_min = 190 
+orange_value_min = 97 
 orange_hsv_min=np.array([orange_hue_min,orange_saturation_min,orange_value_min,])
 
-orange_hue_max = 18
-orange_saturation_max = 229 
-orange_value_max = 174
+orange_hue_max = 20
+orange_saturation_max = 256 
+orange_value_max = 185
 orange_hsv_max=np.array([orange_hue_max,orange_saturation_max,orange_value_max])
 
-mm.append(MM.M_and_M(color_id, "Orange",orangeCircleColor, orange_hsv_min,orange_hsv_max,orangeTextLocation))  # Create Red Tracking Object
+mm.append(MM.M_and_M(color_id, "Orange", max_age, orangeCircleColor, orange_hsv_min,orange_hsv_max,orangeTextLocation))  # Create Red Tracking Object
 
 # YELLOW
 color_id += 1
 yellowCircleColor = (0,255,255)
 yellowTextLocation = (10,100)
 
-yellow_hue_min = 18
-yellow_saturation_min = 71 
-yellow_value_min = 111 
+yellow_hue_min = 27
+yellow_saturation_min = 150 
+yellow_value_min = 90 
 yellow_hsv_min=np.array([yellow_hue_min,yellow_saturation_min,yellow_value_min])
 
-yellow_hue_max =31
-yellow_saturation_max = 215 
-yellow_value_max = 190
+yellow_hue_max = 38
+yellow_saturation_max = 256 
+yellow_value_max = 192
 yellow_hsv_max=np.array([yellow_hue_max,yellow_saturation_max,yellow_value_max])
 
-mm.append(MM.M_and_M(color_id, "Yellow", yellowCircleColor, yellow_hsv_min,yellow_hsv_max,yellowTextLocation))  # Create Red Tracking Object
+mm.append(MM.M_and_M(color_id, "Yellow", max_age, yellowCircleColor, yellow_hsv_min,yellow_hsv_max,yellowTextLocation))  # Create Red Tracking Object
 
 # BLACK
 color_id += 1
 blackCircleColor = (0,0,0)
 blackTextLocation = (10,125)
 
-black_hue_min = 137
-black_saturation_min = 9 
+black_hue_min = 5
+black_saturation_min = 11 
 black_value_min = 0 
 black_hsv_min=np.array([black_hue_min,black_saturation_min,black_value_min])
 
-black_hue_max = 222
-black_saturation_max = 91 
-black_value_max = 64
+black_hue_max = 59
+black_saturation_max = 187  
+black_value_max = 39
 black_hsv_max=np.array([black_hue_max,black_saturation_max,black_value_max])
 
-mm.append(MM.M_and_M(color_id, "Black", blackCircleColor, black_hsv_min,black_hsv_max,blackTextLocation ))  # Create Red Tracking Object
+mm.append(MM.M_and_M(color_id, "Black", max_age, blackCircleColor, black_hsv_min,black_hsv_max,blackTextLocation))  # Create Red Tracking Object
 
 # GREEN
 color_id += 1
 greenCircleColor = (0,255,0)
 greenTextLocation = (10,150)
 
-green_hue_min = 42
-green_saturation_min = 49 
-green_value_min = 42 
+green_hue_min = 46
+green_saturation_min = 50 
+green_value_min = 50 
 green_hsv_min=np.array([green_hue_min,green_saturation_min,green_value_min])
 
 green_hue_max = 76
-green_saturation_max = 173 
-green_value_max = 192
+green_saturation_max = 177 
+green_value_max = 174
 green_hsv_max=np.array([green_hue_max,green_saturation_max,green_value_max])
 
-mm.append(MM.M_and_M(color_id, "Green",greenCircleColor, green_hsv_min,green_hsv_max, greenTextLocation))  # Create Red Tracking Object
+mm.append(MM.M_and_M(color_id, "Green", max_age, greenCircleColor, green_hsv_min,green_hsv_max, greenTextLocation))  # Create Red Tracking Object
 
 ##########
 # Create video camera object and start streaming to it.
@@ -178,9 +180,9 @@ try:
     while(cap.isOpened()):
         # Capture a still image from the video stream
         ret, frame = cap.read() #read a frame
-    
+        
+        """ 
         #Debug code to gauge loop timing
-        '''
         if millis1 != 0: 
             millis2 = millis1
             millis1 = int(round(time.time() * 1000))
@@ -188,10 +190,9 @@ try:
             millis1 = int(round(time.time() * 1000))
         millis = millis1-millis2
         print "MiliSeconds per processing frame: ", millis
-
+        """
         frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
         #frame = imutils.resize(frame, width=min(640, frame.shape[1]))
-        '''
 
         ###############
         # Blur - HSV - Mask - erode - dilate
@@ -205,12 +206,13 @@ try:
         # Loop on each color
         ##########
         for mmColor in mm:
+            print "Debug: ",mmColor.getColor()
 
             # Set up the min and max HSV settings 
             mask=cv2.inRange(hsvframe, mmColor.getHSV_min(), mmColor.getHSV_max())  # Red Mask
             # Get rid of noise
-            mask = cv2.erode(mask, None, iterations=1)
-            mask = cv2.dilate(mask, None, iterations=3)
+            mask = cv2.erode(mask, kernel5, iterations=1)
+            mask = cv2.dilate(mask, kernel5, iterations=3)
     
             # Only return the contours parameter and ignore hierarchy parm, hence [-2]    
             # CHAIN_APPROX_SIMPLE to return less contour points (faster/less memory)
@@ -225,15 +227,16 @@ try:
                 c = max(contours0, key=cv2.contourArea)
                 if c.all() == 0:
                     print "Warning Contours: ",contours0
-                else:
-                    ((x, y), radius) = cv2.minEnclosingCircle(c)
+                ((x, y), radius) = cv2.minEnclosingCircle(c)
+                print "Debug: valid ",mmColor.getColor()
             #else:
                 #print "Warning: Contour Area empty sequence"
 
             #Iterate through the objects found
             mmColor.rstTrackNext()
             for cnt in contours0:
-    
+                print "Debug: contour found in ",mmColor.getColor()
+
                 # Find center of current object
             	M = cv2.moments(cnt)
             	center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
@@ -241,7 +244,7 @@ try:
                 #print (radius)
             	# only proceed if the radius meets a minimum size
             	if radius > min_radius:
-                    #print "DEBUG: radius meets minimum size"
+                    print "DEBUG: radius meets minimum size", radius
     
                     #TODO: Add max size and logic to split into multiple centroids if too big.
 
@@ -252,7 +255,7 @@ try:
             	    cv2.circle(frame, center, int(radius)+4, mmColor.getCircleColor(), 2) 
                     x, y = center
 
-                    print "DEBUG: Color, Center(x,y) = ", mmColor.getColor(), center 
+                    #print "DEBUG: Color, Center(x,y) = ", mmColor.getColor(), center 
                     
                     mmColor.addTrackNext(x,y)    # Used to post process multiple mm objects in next steps
         
